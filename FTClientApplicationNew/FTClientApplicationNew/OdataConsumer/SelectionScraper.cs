353fb3558c8cbbf7075071fdf4e40edb6a2ed2f2
hamzah0553 <hamzah1996@hotmail.com>
2019-11-13 21:34:36 +0100

    Changes

--- FTClientApplicationNew/FTClientApplicationNew/OdataConsumer/SelectionScraper.cs
+++ FTClientApplicationNew/FTClientApplicationNew/OdataConsumer/SelectionScraper.cs
@@ -9,26 +9,28 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
+using FTClientApplication.Model;
+
 namespace FTClientApplication.OdataConsumer
 {
     public class SelectionScraper
     {
         HtmlWeb web = new HtmlWeb();
-        List<string> urlList = new List<string>();
+        List<UrlItem> urlList = new List<UrlItem>();
         List<HtmlDocument> documents = new List<HtmlDocument>();
 
         public SelectionScraper()
         {
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/beu/medlemsoversigt");
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/eru/medlemsoversigt");
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/fiu/medlemsoversigt");
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/fou/medlemsoversigt");
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/kef/medlemsoversigt");
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/sau/medlemsoversigt");
-            urlList.Add("https://www.ft.dk/da/udvalg/udvalgene/ufu/medlemsoversigt");
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/beu/medlemsoversigt", 1));
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/eru/medlemsoversigt", 2));
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/fiu/medlemsoversigt", 4));
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/fou/medlemsoversigt", 5));
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/kef/medlemsoversigt", 3));
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/sau/medlemsoversigt", 7));
+            urlList.Add(new UrlItem("https://www.ft.dk/da/udvalg/udvalgene/ufu/medlemsoversigt", 6));
             for (int i = 0; i < urlList.Count; i++)
             {
-                documents.Add(web.Load(urlList[i]));
+                documents.Add(web.Load(urlList[i].Url));
             }
         }
 
@@ -42,13 +44,15 @@ namespace FTClientApplication.OdataConsumer
                 //thread.Start(i);
                 //listOfThreads.Add(thread);
             }
-            Thread thread = new Thread(GetSelectionMembers);
+            Thread thread = new Thread(CreateSelectionMembers);
+
             thread.Start(0);
         }
 
-        private void GetSelectionMembers(Object data)
+        private void CreateSelectionMembers(Object data)
         {
             string pattern = "^\\d+$";
+            bool shouldInclude = true;
             var tdList = documents[(int)data].DocumentNode.SelectNodes("//td");
             foreach (var node in tdList)
             {
@@ -57,10 +61,43 @@ namespace FTClientApplication.OdataConsumer
                 {
                     if (!Regex.IsMatch(value, pattern))
                     {
-                        Debug.WriteLine(value);
+                        if (shouldInclude)
+                        {
+                            string[] fullname = SplitName(value);
+                            //Debug.WriteLine(fullname[0] + " " + fullname[1]);
+                            shouldInclude = false;
+                        }
+                        else
+                        {
+                            shouldInclude = true;
+                        }
                     }
                 }
             }
+        }
+        private string[] SplitName(string value)
+        {
+            string[] splittedName = value.Split(' ');
+            string firstname = "";
+            string lastname = "";
+            for (int i = 0; i < splittedName.Length; i++)
+            {
+                if (splittedName.Length > 2)
+                {
+                    if (splittedName.Length > i)
+                    {
+                        firstname = splittedName[i];
+                    }
+                    lastname = splittedName[splittedName.Length-1];
+                }
+                if (splittedName.Length < 3)
+                {
+                    firstname = splittedName[0];
+                    lastname = splittedName[1];
+                }
+            }
+            Debug.WriteLine(firstname + " " + lastname);
+            return new string[] { firstname, lastname };
         }
         private string RemoveExcessWhiteSpace(string value)
         {
@@ -71,4 +108,17 @@ namespace FTClientApplication.OdataConsumer
             return newValue;
         }
     }
+
+    public class UrlItem
+    {
+        public UrlItem(string url, int selectionIndex)
+        {
+            Url = url;
+            SelectionIndex = selectionIndex;
+        }
+        public string Url { get; set; }
+        public int SelectionIndex { get; set; }
+       
+    }
+
 }
