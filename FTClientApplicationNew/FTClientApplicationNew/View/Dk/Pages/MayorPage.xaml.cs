374de038907a49c8c4f07c6cf65a431f561713bc
Johnny <john3794@stud.kea.dk>
2019-11-14 18:32:25 +0100

    Mayor added
    - page
    - vm

---
+++ FTClientApplicationNew/FTClientApplicationNew/View/Dk/Pages/MayorPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using FTClientApplication.ViewModel.Dk;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace FTClientApplication.View.Dk.Pages
+{
+    /// <summary>
+    /// Interaction logic for MayorPage.xaml
+    /// </summary>
+    public partial class MayorPage : Page
+    {
+        MayorVM mayor = new MayorVM();
+        public MayorPage()
+        {
+            InitializeComponent();
+           
+        }
+
+    }
+}
---
+++ FTClientApplicationNew/FTClientApplicationNew/ViewModel/Dk/MayorVM.cs
@@ -0,0 +1,27 @@
+﻿using FTClientApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTClientApplication.ViewModel.Dk
+{
+    class MayorVM
+    {
+
+        FTDatabaseEntities entities = new FTDatabaseEntities();
+
+
+        // add new mayor
+
+
+        // return all mayors as List
+
+
+        
+        // add mayor to db 
+
+
+    }
+}
