0ce58297aea8f3555c7d7f8a57a9bce48c3dc6c6
hamzah0553 <hamzah1996@hotmail.com>
2019-11-14 17:07:58 +0100

    GovernmentPage Created for johnny

---
+++ FTClientApplicationNew/FTClientApplicationNew/View/Dk/Pages/GovernmentPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTClientApplication.View.Dk.Pages
+{
+    /// <summary>
+    /// Interaction logic for GovernmentPage.xaml
+    /// </summary>
+    public partial class GovernmentPage : Page
+    {
+        public GovernmentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void governmentBox_DropDownClosed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
